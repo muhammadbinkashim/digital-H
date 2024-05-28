05cc349561a89fad385b1344132f5fc7bea9fbbb
Muhammad-Bin-Kashim <muhammadbinkashim773@gmail.com>
2024-05-28 14:04:27 +0600

    Fixed issue with login feature

--- src/collections/User.ts
+++ src/collections/User.ts
@@ -2,13 +2,27 @@ import { CollectionConfig } from "payload/types";
 
 export const Users: CollectionConfig = {
   slug: "users",
+  auth:{
+    verify: {
+      generateEmailHTML: ({ token }) => {
+        return `<p>Hello plz verify </p>`
+      }
+    },
+  },
+  access: {
+    read: () => true,
+    create: () => true,
+  },
   fields: [
     {
       name: "role",
+      defaultValue: "user",
+      required: true,
+
       type: "select",
       options: [
         { label: "Admin", value: "admin" },
-        { label: "Users", value: "users" },
+        { label: "Users", value: "user" },
       ],
     },
   ],
